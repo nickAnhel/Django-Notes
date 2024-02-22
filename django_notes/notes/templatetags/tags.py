6174cc13240b2a4ae6272380b899823a0c23e729
nickAnhel <anhimovn1@gmail.com>
2024-02-22 23:24:18 +0300

    Fix creating notes with same title Add template tag for rendering notes text in markdown

---
+++ django_notes/notes/templatetags/tags.py
@@ -0,0 +1,11 @@
+from django.utils.safestring import mark_safe
+from django import template
+import markdown
+
+
+register = template.Library()
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
--- django_notes/notes/views.py
+++ django_notes/notes/views.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import MultipleObjectsReturned
+import re
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.text import slugify
@@ -8,24 +8,12 @@ from .forms import NoteForm
 
 
 def note_list(request: HttpRequest) -> HttpResponse:
-    notes = Note.objects.all()
-
+    notes = Note.objects.all()  # .filter(user=request.user)
     return render(request, "note/note_list.html", {"notes": notes})
 
 
 def note_detail(request: HttpRequest, slug) -> HttpResponse:
-    try:
-        note = get_object_or_404(Note, slug=slug)
-    except MultipleObjectsReturned:
-        notes = list(Note.objects.filter(slug=slug))
-        i = 0
-        for note in notes:
-            note.slug += f"-{i + 1}"
-            note.save()
-            i += 1
-
-        note = notes[0]
-
+    note = get_object_or_404(Note, slug=slug)
     return render(request, "note/detail.html", {"note": note})
 
 
@@ -34,9 +22,19 @@ def note_create(request: HttpRequest) -> HttpResponse:
         note_create_form = NoteForm(data=request.POST)
 
         if note_create_form.is_valid():
-            new_note = note_create_form.save(commit=False)
+            new_note: Note = note_create_form.save(commit=False)
             new_note.user = request.user
-            new_note.slug = slugify(note_create_form.cleaned_data["title"])
+
+            for user_note in Note.objects.filter(user=request.user):
+                if user_note.title.startswith(new_note.title):
+                    if re.search(r"\x28\d{1,}\x29", user_note.title):
+                        new_note.title += (
+                            f" ({int(user_note.title[:-1].split('(')[-1]) + 1})"
+                        )
+                    else:
+                        new_note.title += " (1)"
+
+            new_note.slug = slugify(new_note.title)
             new_note.save()
 
             return redirect(new_note.get_absolute_url())
@@ -54,9 +52,11 @@ def note_edit(request: HttpRequest, slug) -> HttpResponse:
         )
 
         if note_edit_form.is_valid():
-            note = note_edit_form.save(commit=False)
+            note: Note = note_edit_form.save(commit=False)
             note.slug = slugify(note_edit_form.cleaned_data["title"])
             note.save()
+
+            return redirect(note.get_absolute_url())
 
     else:
         note_edit_form = NoteForm(instance=Note.objects.get(slug=slug))
